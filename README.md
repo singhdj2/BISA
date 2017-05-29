aa1c079a30801cb2d252bdb6686cf90396147aca
singhdj2 <singhdj2@ksu.edu>
2017-05-29 09:00:44 -0500

    rearranged the project structure

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # BISA 2014-17 HTP GS project
-# Author: Daljit Singh
-# singhdj2@ksu.edu
+## Author: Daljit Singh
+## singhdj2@ksu.edu
 
---
+++ code/rename_RedEdge_files.R
@@ -0,0 +1,48 @@
+# 2017-05-28 
+# Function renames Micasense RedEdge Camera files for database uploading
+# keeps the original MicaSense Camera folder structure intact!
+# Author: singhdj2@ksu.edu
+
+### Note: Function assumes the input argument 'directoryPath' is of MicaSense file structure i.e.
+### Full Flight Directory >> 000NSET Folders >> 000N Folders >> '.tif' image files
+### Run this function after saving a backup copy of your work elsewhere
+
+rename_RE_files <- function(dirPath){
+  if (!is.null(dirPath)) setwd(dirPath)
+  main_dir_list <- list.dirs(full.names = F, recursive = F)
+  cat("*There are",length(main_dir_list),"flight folders in this dataset...", "\n")
+  lapply(main_dir_list, FUN = function(i){ 
+    cat("**Entering", i, "flight folder...","\n")
+    sub_dir_list <- list.dirs(i, full.names = F, recursive = F)
+    dat_list <- list.files(i, pattern = ".dat")
+    if (length(dat_list) != 0) {
+      lapply(dat_list, FUN= function(x){
+        old_dat_path = paste0(i,"/",x)
+        dat_path = paste0(dirPath,"/",i,"_",x)
+        file.rename(from = old_dat_path, to = dat_path)
+      })
+    } 
+    cat("  ***Folder", i, "has", length(sub_dir_list), "sub-folders..","\n")
+    lapply(sub_dir_list, FUN = function(j){
+      cat("    ****Entering sub-folder", j, "of", i, "now...", "\n")
+      file_path <- paste0(i,"/",j)
+      file_list <- list.files(file_path, pattern = ".tif")
+      cat("      *****Sub-folder", "'", j,"'", "has", length(file_list), "image files..","\n")
+      lapply(file_list, FUN = function(k){
+        old_name <- paste0(file_path,"/",k)
+        #new_name <- paste0(dir,"/",dir,"_",sub_dir,"_",file)
+        #file.rename(from = old_name, to = new_name)
+        new_name <- paste0(dirPath,"/",i,"_",j,"_",k)
+        file.rename(from = old_name, to = new_name) # careful though; rename deletes the files from their original location. Alternatively use file.copy
+        #cat(old_name, "\n")
+        #cat(new_name, "\n")
+      })
+    })
+    #delete the copied folders from their original location
+    unlink(i, recursive = TRUE)
+  })
+}
+
+
+## run function on RedEdge flights one by one to make sure everything stays under control!
+system.time(rename_files("~/Downloads/test_rededge1/"))
